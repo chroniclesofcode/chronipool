9753b061f97969e72bdfd248294c0ee339d000e5
Donald Tang <donald.tang.email@googlemail.com>
2024-02-11 09:48:24 +1100

    Init

---
+++ include/thread_pool.hpp
@@ -0,0 +1,12 @@
+#include <iostream>
+namespace chronicles {
+
+class thread_pool {
+public:
+    thread_pool() {
+        std::cout << "created" << std::endl;
+    }
+private:
+};
+
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "thread_pool.hpp"
+
+int main(void) {
+    std::cout << "aloha!" << std::endl;
+    chronicles::thread_pool t;
+    return 0;
+}
