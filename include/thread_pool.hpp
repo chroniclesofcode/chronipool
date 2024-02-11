1d9b37b4e8ec20b9492336bc0b620de615877a92
Donald Tang <donald.tang.email@googlemail.com>
2024-02-12 08:38:18 +1100

    Resolved compiler issues

--- include/thread_pool.hpp
+++ include/thread_pool.hpp
@@ -11,14 +11,63 @@
 
 namespace chronicles {
 
+
 class thread_pool {
+private:
+
+    /* HELPER CLASS DEFINITION */
+
+    // we will have a pointer of generic type to a derived class (of generic
+    // type) which will hide it's template type, so that we can use it
+    // anywhere a std::function can be used. This is to escape the rule that
+    // std::functions can only be of copy-constructible types.
+    struct FnWrapper {
+
+        struct generic_type {
+            virtual void call() = 0;
+            virtual ~generic_type() {}
+        };
+
+        template <typename T>
+        struct erased_type : generic_type {
+            T elem;
+            erased_type(T&& other) : elem{std::move(other)} {}
+            void call() { elem(); }
+        };
+
+        std::unique_ptr<generic_type> curr;
+
+        // FnWrapper can take in any type (via universal reference) and store
+        // it as a pointer to call.
+        template <typename F>
+        FnWrapper(F&& other) : curr(new erased_type<F>(std::move(other))) {}
+        FnWrapper() = default;
+        
+        void operator()(){ curr->call(); }
+
+        // We take their unique_ptr as ours
+        FnWrapper(FnWrapper&& other) : curr{std::move(other.curr)} {}
+        
+        FnWrapper& operator=(FnWrapper&& other) {
+            curr = std::move(other.curr);
+            return *this; 
+        }
+
+        FnWrapper(const FnWrapper& other) = delete;
+    };
+
 public:
 
     /* CONSTRUCTORS */
 
-    thread_pool(unsigned int init_threads = std::thread::hardware_concurrency()) : end_work{0} {
-        for (unsigned int i = 0; i < init_threads; i++) {
-            threads.emplace_back(std::thread(&thread_pool::execute_task, this));
+    thread_pool(unsigned int init_threads = std::thread::hardware_concurrency()) : end_work{false} {
+        try {
+            for (unsigned int i = 0; i < init_threads; i++) {
+                threads.emplace_back(std::thread(&thread_pool::execute_task, this));
+            }
+        } catch(...) {
+            end_work = true;
+            throw;
         }
     }
 
@@ -40,13 +89,16 @@ public:
     auto push_task(F&& func) -> std::future<decltype(func())> {
         using RetType = decltype(func());
         using Package = std::packaged_task<RetType()>;
-        std::shared_ptr<Package> spt = std::make_shared<Package>(func);
-        std::future<RetType> ft = spt->get_future();
+        Package pt(func);
+        std::future<RetType> ft = pt.get_future();
+        FnWrapper wrapped_package(std::move(pt));
         {
             std::lock_guard<std::mutex> lck(mtx_q);
-            task_q.emplace([my_package = spt]() {
-                (*my_package)();
+            
+            task_q.emplace([my_package = std::move(wrapped_package)]() mutable {
+                my_package();
             });
+            
             cv.notify_one();
         }
         return ft;
@@ -75,13 +127,13 @@ private:
 
     void execute_task() {
         while (!end_work) {
-            std::function<void()> func;
+            FnWrapper func;
             {
                 std::unique_lock<std::mutex> lck(mtx_q);
                 cv.wait(lck, [&]() { return !task_q.empty() || end_work; });
                 if (end_work) break;
 
-                func = task_q.front();
+                func = std::move(task_q.front());
                 task_q.pop();
             }
             func();
@@ -96,7 +148,7 @@ private:
     std::vector<std::thread> threads;
     std::mutex mtx_q;
     std::condition_variable cv;
-    std::queue<std::function<void()>> task_q;
+    std::queue<FnWrapper> task_q;
 };
 
 }
