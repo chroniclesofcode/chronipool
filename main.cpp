86c92465d6dd30ca02e036ff9e60421fa420bba3
Donald Tang <donald.tang.email@googlemail.com>
2024-02-12 17:45:53 +1100

    added manual run task

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(void) {
     int x = 69;
     // std::future<int> ft = tp.push_task([](int x) { return x * x; }, 69);
     std::future<int> ft = tp.push_task([&]() { return x * x; });
+    tp.manual_run_task();
     std::cout << ft.get() << std::endl;
     return 0;
 }
